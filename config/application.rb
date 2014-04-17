17fe08e7f67bcb333d51b1d44bf2aabbe3d2f6ac
Wildan <wildan.p@kiranatama.com>
2014-04-17 16:37:29 +0700

    added assets precompile

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module Mysite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    config.assets.initialize_on_precompile = false
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += %w( .svg .eot .woff .ttf)
